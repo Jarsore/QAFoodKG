ce73b41036f5b8ae0f2b8611b3be5b1f06b20866
Jarsore <118436143+Jarsore@users.noreply.github.com>
2024-05-10 13:10:31 +0800

    知识图谱美食问答系统

--- foodQASystem_graph.py
+++ foodQASystem_graph.py
@@ -28,12 +28,51 @@ class FoodQASystemGraph:
         res_sql = self.parser.parser_main(res_classify) # 调用parser_main对内容进行解析
 
         final_answers = self.searcher.search_main(res_sql) # 对内容搜索合适的答案
+
         if not final_answers:
             print(2)
             return answer # 如果没有找到合适的最终答案，返回初始答案
         else:
             print(3)
-            return '\n'.join(final_answers) # 连接字符
+            # 将答案列表转换为一个换行符分隔的字符串
+            answers_str = '\n'.join(final_answers)
+
+            # return '\n'.join(final_answers) # 连接字符
+
+            # 返回一个包含答案字符串和SQL语句的字典
+            return {
+                'answer': answers_str,
+                'res_sql': res_sql
+                        }
+
+
+
+
+
+
+
+    # def chat_main(self, sent):
+    #     answer = '抱歉，没能理解您的问题，我的词汇量有限，请输入更加标准的词语' # 这是初始答案
+    #     res_classify = self.classifier.classify(sent) # 'sent'是用户的输入内容，利用classify函数先对其进行分类
+    #
+    #     if not res_classify:
+    #         print(1)
+    #         return answer # 没有找到对应分类内容，返回初始答案
+    #     ## 到上一步是没有问题的
+    #
+    #     res_sql = self.parser.parser_main(res_classify) # 调用parser_main对内容进行解析
+    #
+    #     final_answers = self.searcher.search_main(res_sql) # 对内容搜索合适的答案
+    #     if not final_answers:
+    #         print(2)
+    #         return answer # 如果没有找到合适的最终答案，返回初始答案
+    #     else:
+    #         print(3)
+    #         # return res_sql
+    #         return '\n'.join(final_answers) # 连接字符
+
+
+
 
 if __name__ == '__main__':
     handler = FoodQASystemGraph()
@@ -41,4 +80,7 @@ if __name__ == '__main__':
         question = input('用户美食咨询:')
         answer = handler.chat_main(question)
         print('美食问答系统:', answer)
+
+
+
 
